48dc77a1dceb537d54162637eb20a4b87b5edc00
ladroid <puatachok@gmail.com>
2020-09-28 14:07:58 +0300

    example of how binding Lua and C++

---
+++ lua/example1.h
@@ -0,0 +1,49 @@
+//
+// Created by Lado on 28/9/20.
+//
+
+#ifndef UNTITLED_EXAMPLE1_H
+#define UNTITLED_EXAMPLE1_H
+
+#include <iostream>
+#include <string>
+
+extern "C" {
+#include "lua/lua.h"
+#include "lua/lauxlib.h"
+#include "lua/lualib.h"
+}
+
+/*
+ * EXAMPLE 1
+ */
+void example1() {
+    //initialize the state
+    lua_State *state = luaL_newstate();
+
+    //load libraries
+    luaL_openlibs(state);
+
+    //do file
+    luaL_dofile(state, "example.lua");
+
+    //get global
+    lua_getglobal(state, "square");
+
+    //square function
+    lua_pushnumber(state, 2);
+
+    //square function arguments
+    lua_pcall(state, 1, 1, 0);
+
+    //square result
+    int result = lua_tonumber(state, -1);
+    std::cout << "result = " << result << std::endl;
+
+    //get global variable
+    lua_getglobal(state, "x");
+    int x = lua_tonumber(state, -1);
+    std::cout << "x = " << x << std::endl;
+}
+
+#endif //UNTITLED_EXAMPLE1_H
---
+++ lua/func.lua
@@ -0,0 +1,14 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lado
+-- Date: 28/9/20
+-- Time: 12:24 am
+-- To change this template use File | Settings | File Templates.
+--
+
+local person = Person.new("Jimmy", 25)
+local m = person:get_name()
+local a = person:get_age()
+print(m, a)
+
+
