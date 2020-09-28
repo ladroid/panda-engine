5077fc4e3b29028804b9f249722d57c8f519fb2b
ladroid <puatachok@gmail.com>
2020-09-28 22:47:12 +0300

    Updating script

--- lua/game.lua
+++ lua/game.lua
@@ -6,11 +6,11 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local FPS = 60
-local frameDelay = math.floor(1000 / FPS)
-
-local frameStart
-local frameTime = 0
+--local FPS = 60
+--local frameDelay = math.floor(1000 / FPS)
+--
+--local frameStart
+--local frameTime = 0
 local game = Game.new()
 game:init("title", "SDL_WINDOWPOS_CENTERED", "SDL_WINDOWPOS_CENTERED", 800, 600, false)
 while(game:running()) do
@@ -19,11 +19,12 @@ while(game:running()) do
     game:handleEvent()
     game:update()
     game:render()
-
-    frameStart = game:get_ticks() - frameStart
-    if frameDelay > frameTime then
-        game:delay(frameDelay - frameTime)
-    end
 end
 game:clean()
+--    frameStart = game:get_ticks() - frameStart
+--    if frameDelay > frameTime then
+--        sleep(frameDelay - frameTime)
+--    end
+--end
+--game:clean()
 
